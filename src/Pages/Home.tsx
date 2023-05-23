f9004fd65c094d2951e6ef1e78c62f51dd1d04b2
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-05-23 16:12:32 -0400

    added main page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import "../styles.scss";
+import { Button } from "@mui/material";
 
 const Home: React.FC = () => {
+  const navigate = useNavigate()
+
   return (
     <div className="container">
       <div className="home home--container">
@@ -9,6 +13,7 @@ const Home: React.FC = () => {
         <h3 className="home home__slogan">
           A POWERFUL METRIC VISUALIZATION TOOL FOR gRPC
         </h3>
+        <Button variant="contained" onClick={() => navigate('/Main')}>Start Seeking</Button>
       </div>
     </div>
   );
---
+++ src/Pages/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
