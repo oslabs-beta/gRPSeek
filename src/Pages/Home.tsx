44a38c3d3b2e2a8397902a0ffb4fac6ec95ef048
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-05-23 15:07:24 -0400

    added basic styling to front page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "../styles.scss";
 
-const Home:React.FC = () => {
-    return (
-        <></>
-    )
-}
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="home home--container">
+        <h1 className="home home__title">gRPSeek</h1>
+        <h3 className="home home__slogan">
+          A POWERFUL METRIC VISUALIZATION TOOL FOR gRPC
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
