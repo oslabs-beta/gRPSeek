ebb420aa34e0c3e54a49db63cb2ba4572ccc8a13
johncamilomcconnell <johncmcconnell@gmail.com>
2023-05-23 16:12:29 -0400

    some box comps

--- src/Pages/Client.tsx
+++ src/Pages/Client.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import NavBar from ".././client/components/NavBar"
+import BoxMetric from ".././client/components/Box"
 
 const Client:React.FC = () => {
     return (
         <>
+        <div>
         <NavBar></NavBar>
         <h1>
             hello world from Client
         </h1>
+        <BoxMetric />
+        </div>
         </>
     )
 }
