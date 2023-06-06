9bd321cb6dfb8aeff60c37a95e6042d9d04159b2
Kenny <kennykn@outlook.com>
2023-06-05 22:27:51 -0400

    added example gRPC app

---
+++ example/greeter_client.js
@@ -0,0 +1,123 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const PROTO_PATH = __dirname + '/protos/helloworld.proto';
+
+const parseArgs = require('minimist');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+// function main() {
+//   const argv = parseArgs(process.argv.slice(2), {
+//     string: 'target'
+//   });
+//   const target;
+//   if (argv.target) {
+//     target = argv.target;
+//   } else {
+//     target = 'localhost:50051';
+//   }
+//   const client = new hello_proto.Greeter(target,
+//     grpc.credentials.createInsecure());
+//   const user;
+//   if (argv._.length > 0) {
+//     user = argv._[ 0 ];
+//   } else {
+//     user = 'world';
+//   }
+//   client.sayHello({ name: user }, function (err, response) {
+//     console.log('Greeting:', response.message);
+//   });
+// }
+function main() {
+  const client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+  client.sayHello({ name: 'Kenny' }, function (err, response) {
+    console.log('Greetings:', response.message);
+  });
+  client.sayHelloAgain({ name: 'Miri' }, function (err, response) {
+    console.log('Greetings:', response.message)
+  })
+}
+
+function invoke() {
+  const client2 = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+  client2.sayHello({ name: 'Miri' }, function (err, response) {
+    console.log('Greetings:', response.message);
+  });
+}
+main()
+// module.exports = invoke;
+
+// function startLoopWithTimer() {
+//   const startTime = new Date()
+//   console.log("START:", startTime)
+//   let counter = 0;
+//   const intervalId = setInterval(() => {
+//     // Perform your desired actions inside the loop
+//     main()
+//     counter++;
+
+//     // Once 20 iterations happen 
+//     if (counter === 30) {
+//       clearInterval(intervalId);
+//       const endTime = new Date();
+//       console.log(endTime);
+//       const duration = (endTime - startTime) / 1000;
+//       // Moves it over 2 decimal places pretty kool
+//       console.log(`Elapsed time: ${duration.toFixed(2)} seconds`);
+
+//       console.log(`Loop stopped after ${counter} iterations`);
+//     }
+//   }, 1000); // Set the interval duration (in milliseconds)
+// }
+
+// // Start the loop with a timer
+// startLoopWithTimer();
+
+
+// // main();
+
+// // Testing the function invocation 10 times back to back 
+// // const startTime = new Date()
+// // console.log("START: ", startTime)
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // main()
+// // const endTime = new Date();
+// // const duration = (endTime - startTime) / 1000;
+// // console.log(`Elapsed time: ${duration.toFixed(2)} seconds`);
+
+
---
+++ example/greeter_server.js
@@ -0,0 +1,102 @@
+
+// ============== gRPC MODULES ====================
+const PROTO_PATH = __dirname + '/protos/helloworld.proto';
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// ============= EXPRESS =======================
+const express = require('express');
+const app = express();
+
+// =========== PROMETHEUS ============================
+const { collectDefaultMetrics, register } = require('prom-client');
+const { Counter, Histogram, Summary } = require('prom-client'); // Custom Metrics 
+collectDefaultMetrics()
+
+// ============Creating custom metrics for gRPC ==================
+const grpcRequestCounter = new Counter({
+  name: 'grpc_server_requests_total',
+  help: 'Total number of gRPC requests received',
+  labelNames: [ 'method' ],
+});
+const grpcMethodDurationHistogram = new Histogram({
+  name: 'grpc_server_method_duration_seconds',
+  help: 'Duration of gRPC methods in seconds',
+  labelNames: [ 'method' ],
+  buckets: [ 0.1, 0.5, 1, 2, 5 ],
+});
+const grpcMethodLatencySummary = new Histogram({
+  name: 'grpc_server_method_latency_seconds',
+  help: 'Latency of gRPC methods in seconds',
+  labelNames: [ 'method' ],
+  percentiles: [ 0.1, 0.3, .4, 0.5, 0.9, 0.99 ],
+});
+const grpcRequestSizeHistogram = new Histogram({
+  name: 'grpc_server_request_size_bytes',
+  help: 'Size of incoming gRPC requests in bytes',
+  labelNames: [ 'method' ],
+  buckets: [ 100, 500, 1000, 2000 ],
+});
+
+// =====================================================
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  console.log('Received a Request: ', call.request.name)
+
+  //====================== Implementing Custom Metrics ==========================
+  grpcRequestCounter.inc({ method: 'sayHello' }); // Increment the request counter
+  //============= Measure the method duration=================
+  const startTime = process.hrtime();
+  const endTime = process.hrtime(startTime);
+  const durationInSeconds = endTime[ 0 ] + endTime[ 1 ] / 1e9;
+  // ======================================================
+  grpcMethodDurationHistogram.observe({ method: 'sayHello' }, durationInSeconds);
+  grpcMethodLatencySummary.observe({ method: 'sayHello' }, durationInSeconds);
+  // ============ RESPONSE FROM gRPC SERVER ==================
+  const response = { message: 'Hello from Server' }
+  callback(null, response)
+  // =================================================
+}
+// 
+function sayHelloAgain(call, callback) {
+  callback(null, { message: 'Hello again, ' + call.request.name });
+}
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+function main() {
+  const server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, {
+    sayHello: sayHello,
+    sayHelloAgain: sayHelloAgain
+  });
+  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+    console.log('gRPC server started on: 0.0.0.0:50051')
+  });
+}
+// ========== Prometheus/Express ===========
+const PORT = 9090;
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  const metrics = await register.metrics();
+  res.end(metrics);
+});
+app.listen(PORT, () => console.log(`Prometheus metrics server listening on ${PORT}`))
+main();
