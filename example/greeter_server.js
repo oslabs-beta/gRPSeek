c72af51f38cadc19d33fa52e0c719aa8bab11f9f
msoncsswe <miris11260@gmail.com>
2023-06-08 11:25:07 -0400

    pulling altest dev code

--- example/greeter_server.js
+++ example/greeter_server.js
@@ -40,18 +40,7 @@ const grpcRequestSizeHistogram = new Histogram({
   labelNames: [ 'method' ],
   buckets: [ 100, 500, 1000, 2000 ],
 });
-const grpcMethodLatencySummary = new Summary({
-  name: 'grpc_server_method_latency_seconds',
-  help: 'Latency of gRPC methods in seconds',
-  labelNames: [ 'method' ],
-  percentiles: [ 0.1, 0.3, .4, 0.5, 0.9, 0.99 ],
-});
-const grpcRequestSizeHistogram = new Histogram({
-  name: 'grpc_server_request_size_bytes',
-  help: 'Size of incoming gRPC requests in bytes',
-  labelNames: [ 'method' ],
-  buckets: [ 100, 500, 1000, 2000 ],
-});
+
 
 // =====================================================
 const packageDefinition = protoLoader.loadSync(
