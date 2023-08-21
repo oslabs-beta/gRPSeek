6dc5e3892cde2ac297869b96183f532f6b2c6dd9
Kenny Nguyen <kenny@Kennys-Laptop.local>
2023-08-21 14:42:46 -0700

    feat: add grpc-app

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,2 +1,3 @@
 # !/bin/bash
 npx proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=proto/ proto/*.proto
+ 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './styles.css';
-import { TestingFetch } from './testingFetch';
+
 export const App = () => {
   return (
     <div>
       <h1>Bangarang</h1>
       <p>{process.env.NODE_ENV}</p>
       <p>{process.env.name}</p>
-      <TestingFetch />
     </div>
   );
 };
