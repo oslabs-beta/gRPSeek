0bfa35a1347dbcfa877bd0dcaba68828767f30f7
Ariel Lin <ariellin980@gmail.com>
2023-09-05 14:27:05 -0400

    class MetricInterceptor

--- grpc-client/grpc-client.ts
+++ grpc-client/grpc-client.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import { ProtoGrpcType } from '../proto/helloworld';
-import * as ClientInterceptor from '../server/loadTester';
+import MetricInterceptor from '../server/loadTester';
 
 const PORT = 8082;
 const PROTO = '../proto/helloworld.proto';
@@ -15,8 +15,10 @@ const client = new grpcObj.greeterPackage.Greeter(
   `0.0.0.0:${PORT}`, grpc.credentials.createInsecure(),
 )
 
+let clientInterceptor = new MetricInterceptor();
+
 function main() {
-  client.SayHello({ name: "Kenny" }, { interceptors: [ClientInterceptor.interceptor] }, (err, res) => {
+  client.SayHello({ name: "Kenny" }, { interceptors: [clientInterceptor.interceptor] }, (err, res) => {
     if (err) {
       console.log('error', err)
       return;
@@ -28,5 +30,5 @@ function main() {
 for (let i = 0; i < 100; i++) {
   main();
 }
-console.log('Finished calls: ', ClientInterceptor.numCalls);
-console.log('Number of failed requests: ', ClientInterceptor.numErrors);
+console.log('Finished calls: ', clientInterceptor.numCalls);
+console.log('Number of failed requests: ', clientInterceptor.numErrors);
